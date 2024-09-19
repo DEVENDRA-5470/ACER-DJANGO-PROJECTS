724cb5637601a20dcf8686482fa4a0062c82c642
DEV <ysdevn@gmail.com>
2024-09-19 16:39:46 +0530

    sep19 wd

--- Syntax_Error/all_apps/users/urls.py
+++ Syntax_Error/all_apps/users/urls.py
@@ -3,4 +3,5 @@ from all_apps.users.views import *
 
 urlpatterns=[
     path('',home,name="home"),
+    path('sign-up/',sign_up,name="sign-up")
 ]
--- Syntax_Error/all_apps/users/views.py
+++ Syntax_Error/all_apps/users/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 
+def sign_up(request):
+    if request.method=="POST":
+        user_name=request.POST.get('username')
+        user_email=request.POST.get('email-id')
+        user_first_name=request.POST.get('first-name')
+        user_last_name=request.POST.get('last-name')
+        user_pass1=request.POST.get('Password')
+        user_pass2=request.POST.get('Confirm-Password')
+        print(user_email,user_first_name,user_name)
+        
+    else:
+        pass
+
