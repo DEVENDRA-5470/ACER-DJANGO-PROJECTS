3d1d039c78bee30f972d4d3ef8a3995e503065ef
DEV <ysdevn@gmail.com>
2024-10-03 08:34:23 +0530

    its dev

---
+++ SYNTAX_ERROR1/Syntax_Error/all_apps/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from all_apps.users.views import *
+
+urlpatterns=[
+    path('',home,name="home"),
+    path('sign-up/',sign_up,name="sign-up"),
+    path('sign-in/',sign_in,name="sign-in"),
+    path('profile/', profile, name="profile"),
+    path('profiles/update/<int:pk>/', ProfileUpdateView.as_view(), name='profile_update'),
+    path('user-logout/',user_logout,name='user-logout')
+
+    
+]
